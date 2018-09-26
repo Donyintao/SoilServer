547c83a9acc7cc5f2ffec7265d6ed873bf8380cb
Donyintao <15600685705@163.com>
2018-09-26 15:21:27 +0800

    添加API模块

---
+++ API/urls.py
@@ -0,0 +1,13 @@
+# -*- coding: UTF-8 -*-
+
+from django.conf.urls import url, include
+from django.contrib import admin
+from . import views
+
+urlpatterns = [
+	url(r'^idc_valid', views.IDC_Valid, name='idc_valid'),
+	url(r'^cabinet_valid', views.CabinetValid, name='cabinet_valid'),
+	url(r'^group_valid/', views.GroupValid, name='group_valid'),
+    url(r'^hosts_valid/', views.HostsValid, name='hosts_valid'),
+	url(r'^type_valid/', views.FaultTypeValid, name='type_valid'),
+]
