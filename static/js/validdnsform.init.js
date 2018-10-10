7270e5a39905f2319bf418f60dcc437c40050956
Donyintao <15600685705@163.com>
2018-10-11 00:38:36 +0800

    SaltServer 静态目录js代码更新

--- static/js/validdnsform.init.js
+++ static/js/validdnsform.init.js
@@ -17,7 +17,7 @@ $(function () {
       },
       callback: function (result) {
         if (result) {
-          $.post("/dns/delete/", {id: id}, function (result,status) {
+          $.post("/dns/dns_delete/", {id: id}, function (result,status) {
             if (status == 'success') {
               var result = $.parseJSON(result);
               if (result.status == 'true') {
@@ -56,18 +56,24 @@ $(function () {
         validators: {
           notEmpty: {
             message: '解析地址不能为空.'
-          }
+          },
+          regexp: {
+            regexp: /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/,
+            message: '请输入正确的IPV4网段，例如: XX.XX.XX.XX.'
+          },
         }
+        
       },
     }
   });
   $('.add_dnsValid').click(function () {
     $('#add_dnsFrom').bootstrapValidator('validate');
     if($('#add_dnsFrom').data("bootstrapValidator").isValid()){
-      var host = $('#host').val()
-      var type = $('#type').val()
-      var data = $('#data').val()
-      $.post('/dns/add/', {host: host, type: type, data: data}, function (result, status) {
+      var id = $(this).attr('CurlId');
+      var host = $('#host').val();
+      var type = $('#type').val();
+      var data = $('#data').val();
+      $.post('/dns/dns_add/', {id: id, host: host, type: type, data: data}, function (result, status) {
         if (status == 'success') {
           var result = $.parseJSON(result);
           if (result.status == 'true') {
@@ -116,14 +122,16 @@ $(function () {
     $('#up_dnsForm').bootstrapValidator('validate');
     if($('#up_dnsForm').data("bootstrapValidator").isValid()){
       var id = $(this).attr('CurlId');
-      var host = $('#host').val()
-      var type = $('#type').val()
-      var data = $('#data').val()
-      $.post('/dns/update/', {id: id, host: host, type: type, data: data},  function (result, status) {
+      var zone = $(this).attr('ZoneId');
+      var name = $(this).attr('Zone');
+      var host = $('#host').val();
+      var type = $('#type').val();
+      var data = $('#data').val();
+      $.post('/dns/dns_update/', {id: id, zone: zone, host: host, type: type, data: data},  function (result, status) {
         if (status == 'success') {
           var result = $.parseJSON(result);
           if (result.status == 'true') {
-            bootbox.alert("<h4 class='text-center'>数据更新成功!</h4>", function(){ self.location='/dns/list/'; });
+            bootbox.alert("<h4 class='text-center'>数据更新成功!</h4>", function(){ self.location='/dns/setting/?name=' + name; });
           }
         }
       });
@@ -133,5 +141,42 @@ $(function () {
   });
   $('.up_dnsReset').click(function () {
     $('#up_dnsFrom').data('bootstrapValidator').resetForm(true);
+  });
+});
+
+// 添加zone信息
+$(function () {
+  $('#add_zoneFrom').bootstrapValidator({
+    message: 'This value is not valid',
+    fields: {
+      name: {
+        validators: {
+          notEmpty: {
+            message: '域名记录不能为空.'
+          }
+        }
+      },
+    }
+  });
+  $('.add_zoneValid').click(function () {
+    $('#add_zoneFrom').bootstrapValidator('validate');
+    if ($('#add_zoneFrom').data("bootstrapValidator").isValid()) {
+      var name = $('#name').val();
+      $.post('/dns/zone_add/', {name: name}, function (result, status) {
+        if (status == 'success') {
+          var result = $.parseJSON(result);
+          if (result.status == 'true') {
+            bootbox.alert("<h4 class='text-center'>数据添加成功!</h4>", function () {
+              window.location.reload();
+            });
+          }
+        }
+      });
+    } else {
+      return;
+    }
+  });
+  $('.add_dnsReset').click(function () {
+    $('#add_dnsFrom').data('bootstrapValidator').resetForm(true);
   });
 });
